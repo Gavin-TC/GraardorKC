a005d483813ee7e21a2bb30ed1dd8e52942bb15a
Gavin-TC <gtclonts@gmail.com>
2024-04-28 16:58:04 -0400

    Currently counting boss/minion KC if player is in the Bandos room

--- src/main/java/com/gkc/GKCPlugin.java
+++ src/main/java/com/gkc/GKCPlugin.java
@@ -2,10 +2,12 @@ package com.gkc;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
+
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.GameState;
+import net.runelite.api.*;
+import net.runelite.api.coords.WorldArea;
+import net.runelite.api.events.ActorDeath;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
@@ -20,11 +22,24 @@ import net.runelite.client.plugins.PluginDescriptor;
 )
 public class GKCPlugin extends Plugin
 {
-	@Inject
-	private Client client;
+	private final WorldArea bandosRoom = new WorldArea(2864, 5351, 13, 20, 2);
 
 	@Inject
+	private Client client;
+	@Inject
 	private GKCConfig config;
+
+	@Getter
+	private int bossKC;
+	@Getter
+	private int rangeMinionKC;
+	@Getter
+	private int meleeMinionKC;
+	@Getter
+	private int mageMinionKC;
+
+	@Getter
+	private boolean canCount = false;  // Determines whether the plugin will start counting kills
 
 	@Override
 	protected void startUp() throws Exception
@@ -35,7 +50,10 @@ public class GKCPlugin extends Plugin
 	@Override
 	protected void shutDown() throws Exception
 	{
-		log.info("GKC stopped!");
+		bossKC = 0;
+		rangeMinionKC = 0;
+		meleeMinionKC = 0;
+		mageMinionKC = 0;
 	}
 
 
@@ -44,8 +62,41 @@ public class GKCPlugin extends Plugin
 	{
 		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
 		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "GKC says " + config.greeting(), null);
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "isInBandosRoom: " + isInBandosRoom(), "GKC");
 		}
+	}
+
+	@Subscribe
+	public void onActorDeath(ActorDeath actorDeath) {
+		Actor actor = actorDeath.getActor();
+		if (isInBandosRoom() || true) {
+			switch (actor.getName()) {
+				case "General Graardor":
+					bossKC++;
+					break;
+				case "Sergeant Grimspike":
+					rangeMinionKC++;
+					break;
+				case "Sergeant Strongstack":
+					meleeMinionKC++;
+					break;
+				case "Sergeant Steelwill":
+					mageMinionKC++;
+					break;
+				default:
+					log.info("Enemy name: " + actor.getName());
+					break;
+			}
+		}
+	}
+
+	public boolean isInBandosRoom() {
+		Player localPlayer = client.getLocalPlayer();
+		return localPlayer != null && localPlayer.getWorldArea().intersectsWith(bandosRoom);
+	}
+
+	public int getMinionKC() {
+		return rangeMinionKC + meleeMinionKC + mageMinionKC;
 	}
 
 	@Provides
